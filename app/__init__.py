55d2a09a97e34c358de945f0834a194f6fb0273d
Duong Van Khanh <dvkhanh.work@gmail.com>
2024-01-25 01:37:24 +0700

    fix ho cai gio hang

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from urllib.parse import quote
-
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.secret_key = '21137affa59a4dd08b708dcf106c724f9'
 
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/e_commerce?charset=utf8mb4" % quote('Myca@1236')
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/e_commerce?charset=utf8mb4" % quote('GOT123456')
+
+
+login = LoginManager(app)
 
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
--- app/index.py
+++ app/index.py
@@ -1,8 +1,28 @@
-from app import app, dao
-from flask import render_template, request
-from flask_sqlalchemy import SQLAlchemy
-
+import login as login
+from flask_login import logout_user, login_user
+from app import app, dao, login, utils
+from flask import render_template, request, session, redirect, url_for, jsonify
 from app.models import UserRoleEnum
+
+
+@app.route('/api/cart', methods=['POST'])
+def add_cart():
+    cart = session.get('cart')
+    if cart is None:
+        cart = {}
+    data = request.json
+    id = str(data.get("id"))
+    if id in cart:
+        cart[id]['quantity'] = cart[id]['quantity'] + 1
+    else:
+        cart[id] = {
+            "id": id,
+            "name": data.get("name"),
+            "price": data.get("price"),
+            "quantity": 1
+        }
+        session['cart'] = cart
+    return jsonify(utils.count_cart(cart))
 
 
 @app.route('/')
@@ -22,12 +42,30 @@ def product_detail(product_id):
 
 @app.route('/user_login', methods=['POST', 'GET'])
 def user_login():
+    user_role_enum_values = UserRoleEnum.__members__.values()
     if request.method.__eq__('POST'):
         username = request.form.get('username')
         password = request.form.get('password')
-
-    user_role_enum_values = UserRoleEnum.__members__.values()
+        role = request.form.get('role')
+        session['role'] = role
+        user = dao.authenticate_user(username=username, password=password, role=role)
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
     return render_template('user_login.html', user_role_enum_values=user_role_enum_values)
+
+
+@app.route('/user-logout')
+def user_logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@login.user_loader
+def load_user(user_id):
+    role = session['role']
+    user = dao.get_user_by_id(user_id, role)
+    return user
 
 
 if __name__ == '__main__':
